526a03f6de245eb377982bff650db7a60489f937
Minik ArchDesktop <>
2024-03-10 21:48:30 +0100

    fix: removed build files

--- build/hello_blink.d
+++
@@ -1,9 +0,0 @@
-build/hello_blink.o: src/hello_blink.c \
- /usr/lib/gcc/arm-none-eabi/13.2.0/include/stdbool.h \
- /usr/lib/gcc/arm-none-eabi/13.2.0/include/stdint.h \
- /usr/arm-none-eabi/include/stdint.h \
- /usr/arm-none-eabi/include/machine/_default_types.h \
- /usr/arm-none-eabi/include/sys/features.h \
- /usr/arm-none-eabi/include/_newlib_version.h \
- /usr/arm-none-eabi/include/sys/_intsup.h \
- /usr/arm-none-eabi/include/sys/_stdint.h src/../include/tm4c123gh6pm.h
