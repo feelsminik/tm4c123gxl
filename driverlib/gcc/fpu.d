c97eb981ea6f3e6c57532b6abb0d3f19f2337464
Minik ArchDesktop <>
2024-03-10 20:19:03 +0100

    fix: remove unnecessary vendor libs/files

--- driverlib/gcc/fpu.d
+++
@@ -1,8 +0,0 @@
-gcc/fpu.o: fpu.c /usr/lib/gcc/arm-none-eabi/9.3.0/include/stdint.h \
- /usr/arm-none-eabi/include/stdint.h \
- /usr/arm-none-eabi/include/machine/_default_types.h \
- /usr/arm-none-eabi/include/sys/features.h \
- /usr/arm-none-eabi/include/_newlib_version.h \
- /usr/arm-none-eabi/include/sys/_intsup.h \
- /usr/arm-none-eabi/include/sys/_stdint.h ../inc/hw_nvic.h \
- ../inc/hw_types.h ../driverlib/fpu.h
